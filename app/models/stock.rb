ae0c994838bd602cff28bfab7884675708f658e7
parluciaga <parluciaga@yahoo.com>
2021-04-07 08:33:20 -0400

    Enhanced account edit view

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,20 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iexcloud[:publishable_token], 
                                     endpoint: 'https://sandbox.iexapis.com/v1')
-    client.price(ticker)
+    begin
+      new(ticker: ticker, name: client.company(ticker).company_name, last_price: client.price(ticker))
+    rescue => exception
+      return nil
+    end
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
